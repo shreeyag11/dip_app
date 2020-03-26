de071e326a3108c5f82717a863d6dce5fd6ffe62
Dhruv Dhamani <dhruv.dhamani@gmail.com>
2020-03-26 16:06:37 -0400

    plugin conforming to eslint

--- src/main.js
+++ src/main.js
@@ -6,12 +6,14 @@ import 'buefy/dist/buefy.css';
 import App from './App.vue';
 import lib from '../crate/Cargo.toml';
 
+/* eslint-disable */
 const myLib = {
   install: function(Vue, options) {
     lib.init();
     Vue.prototype.$lib = lib;
   }
 };
+/* eslint-enable */
 
 Vue.use(myLib);
 Vue.use(Buefy);
