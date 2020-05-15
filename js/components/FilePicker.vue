a9da057df426b6cf16a30a50cfe386ca35b9b27c
Dhruv Dhamani <dhruv.dhamani@gmail.com>
2020-05-15 12:16:10 -0400

    for shreeya

---
+++ js/components/FilePicker.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <section>
+      <b-field>
+        <b-upload
+          v-model="dropFile"
+          drag-drop
+          @input="parse"
+        >
+          <section class="section">
+            <div class="content has-text-centered">
+              <p>
+                <b-icon
+                  icon="upload"
+                  size="is-large"
+                />
+              </p>
+              <p
+                v-if="dropFile"
+                class="is-size-4"
+              >
+                Successfully read {{ dropFile.name }}
+              </p>
+              <p
+                v-else
+                class="is-size-4"
+              >
+                Drop your files here or click to upload
+              </p>
+            </div>
+          </section>
+        </b-upload>
+      </b-field>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['pageName'],
+  data() {
+    return {
+      dropFile: undefined,
+    };
+  },
+  methods: {
+    parse() {
+      this.$store.dispatch('PARSE_FILE', {
+        file: this.dropFile,
+        type: this.pageName,
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.upload {
+  width: 100%;
+  display: block;
+}
+</style>
---
+++ js/components/Log.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="container">
+    <article
+      id="logContainer"
+      :class="logMessageClass"
+    >
+      <div
+        id="hint"
+        class="message-header"
+      >
+        <p>Log</p>
+      </div>
+      <div
+        id="errorLog"
+        class="message-body"
+      >
+        <div v-if="$store.state[pageName].fileParseSuccessful">
+          <p v-for="msg in logMessage">
+            {{ msg }}
+          </p>
+        </div>
+        <pre v-else>{{ logMessage }}</pre>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['pageName'],
+  computed: {
+    fileParseErrors() {
+      let errors;
+      if (this.$store.state[this.pageName].fileParseErrors) {
+        if (this.$store.state[this.pageName].fileParseErrors.split) {
+          errors = this.$store.state[this.pageName].fileParseErrors.split(
+            '#!@',
+          );
+        } else {
+          errors = [this.$store.state[this.pageName].fileParseErrors];
+        }
+
+        if (errors.length >= 15) {
+          errors = errors.slice(0, 15);
+          errors.push('😭 And I stopped counting after the first 15 errors');
+        }
+
+        return errors.join('\n');
+      }
+      return undefined;
+    },
+    logMessageClass() {
+      if (this.$store.state[this.pageName].fileParseSuccessful) return 'message is-primary';
+      if (this.fileParseErrors) return 'message is-danger';
+      return 'message is-warning';
+    },
+    logMessage() {
+      if (this.$store.state[this.pageName].fileParseSuccessful) {
+        if (this.pageName === 'viewer') {
+          return [
+            'File parsed successfully.',
+            `The parsed file type is ${
+              this.$store.state[this.pageName].file.file_type
+            }`,
+            `The parsed height is ${
+              this.$store.state[this.pageName].file.height
+            }`,
+            `The parsed width is ${this.$store.state[this.pageName].file.width}`,
+          ];
+        }
+        if (this.pageName === 'ass1') {
+          return [
+            `Successfully parsed ${this.$store.state[this.pageName].file.name}`,
+          ];
+        }
+      }
+      if (this.$store.state[this.pageName].fileParseErrors) {
+        return this.fileParseErrors;
+      }
+      return undefined;
+    },
+  },
+};
+</script>
